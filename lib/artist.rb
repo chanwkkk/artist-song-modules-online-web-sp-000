7692683c5b59080451181a4e956fac37dfbd23e4
Yingqi Chen <chanwk18@hotmail.com>
2019-04-15 17:53:35 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -3,9 +3,11 @@ require 'pry'
 class Artist
   attr_accessor :name
   attr_reader :songs
-  extend Memorable
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
   extend Findable
   include Paramable
+  
 
   @@artists = []
 
